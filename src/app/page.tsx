ac1cb5b52c648c9fa6d14aaf82de64558419b1d2
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-12-09 22:38:39 +0000

    Home page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Toolbelt</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold text-white">toolbelt</h1>
+      <div className="flex flex-col gap-2 p-8 text-white font-bold">
+        <Link className="hover:text-gray-400" href="/">
+          habbit </Link>
+        <Link className="hover:text-gray-400" href="/">
+          notes </Link>
+        <Link className="hover:text-gray-400" href="/">
+          pomodoro </Link>
+      </div>
     </div>
   );
 }
