90904232b9ef261b22069636880a3a812ff8de42
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-12-14 22:46:35 +0000

    Routes for tools made

---
+++ src/app/(tools)/habit/page.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center bg-gray-900 h-screen">
+                <div className="flex flex-row items-center justify-center gap-4 text-white">
+                    <Link href="/">
+                        <h1 className="text-6xl font-bold text-white">toolbelt</h1>
+                    </Link>
+                    <div className="flex flex-col gap-2 p-8 text-white font-bold">
+                        <Link className="hover:text-gray-400" href="/notes">
+                            notes </Link>
+                        <Link className="hover:text-gray-400" href="/pomodoro">
+                            pomodoro </Link>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,15 @@ export default function Home() {
     <>
       <div className="flex flex-col items-center justify-center bg-gray-900 h-screen">
         <div className="flex flex-row items-center justify-center gap-4 text-white">
-          <h1 className="text-6xl font-bold text-white">toolbelt</h1>
+          <Link href="/">
+            <h1 className="text-6xl font-bold text-white">toolbelt</h1>
+          </Link>
           <div className="flex flex-col gap-2 p-8 text-white font-bold">
-            <Link className="hover:text-gray-400" href="/">
-              habbit </Link>
-            <Link className="hover:text-gray-400" href="/">
+            <Link className="hover:text-gray-400" href="/habit">
+              habit </Link>
+            <Link className="hover:text-gray-400" href="/notes">
               notes </Link>
-            <Link className="hover:text-gray-400" href="/">
+            <Link className="hover:text-gray-400" href="/pomodoro">
               pomodoro </Link>
           </div>
         </div>
