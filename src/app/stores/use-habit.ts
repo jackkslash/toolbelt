30aec906bb061871c5bb5eea50fca1f5ef1bbfde
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-12-21 15:32:42 +0000

    Stores updated

--- src/app/stores/use-habit.ts
+++ src/app/stores/use-habit.ts
@@ -7,14 +7,30 @@ type Habit = {
 
 type HabitStore = {
     habits: Habit[]
-    addHabit: (habit: Habit) => void
+    addHabit: (name: string) => void
+    deleteHabit: (id: number) => void
+    updateHabit: (id: number, name: string) => void
 }
 
 export const useHabit = create<HabitStore>((set) => ({
     habits: [],
-    addHabit: async (habit: any) => {
+    addHabit: async (name: string) => {
+        const habit: Habit = {
+            id: Math.random(),
+            name
+        }
         set((state: { habits: any; }) => ({
             habits: [...state.habits, habit]
+        }))
+    },
+    deleteHabit: async (id: number) => {
+        set((state: { habits: any; }) => ({
+            habits: state.habits.filter((habit: { id: number; }) => habit.id !== id)
+        }))
+    },
+    updateHabit: async (id: number, name: string) => {
+        set((state: { habits: any; }) => ({
+            habits: state.habits.map((habit: { id: number; }) => habit.id === id ? { ...habit, name } : habit)
         }))
     }
 }));
